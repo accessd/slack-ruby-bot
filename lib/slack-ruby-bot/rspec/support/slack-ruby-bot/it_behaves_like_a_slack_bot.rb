fe889753f3b78e87a975a4fa70b5558b551a1c09
Accessd <accessd0@gmail.com>
2016-04-26 20:41:25 +0300

    ability to pass token through configuration

--- lib/slack-ruby-bot/rspec/support/slack-ruby-bot/it_behaves_like_a_slack_bot.rb
+++ lib/slack-ruby-bot/rspec/support/slack-ruby-bot/it_behaves_like_a_slack_bot.rb
@@ -1,13 +1,25 @@
 shared_examples 'a slack ruby bot' do
   context 'not configured' do
     before do
+      SlackRubyBot.configure do |c|
+        c.token = nil
+      end
       @slack_api_token = ENV.delete('SLACK_API_TOKEN')
     end
     after do
       ENV['SLACK_API_TOKEN'] = @slack_api_token
+      SlackRubyBot.configure do |c|
+        c.token = 'testtoken'
+      end
     end
     it 'requires SLACK_API_TOKEN' do
       expect { subject }.to raise_error RuntimeError, "Missing ENV['SLACK_API_TOKEN']."
+    end
+  end
+
+  context 'token configured' do
+    it 'does not require SLACK_API_TOKEN' do
+      expect { subject }.not_to raise_error
     end
   end
 end
